Endscape-Coding/EN-OS-Calamares

commit a9c6f3e71875f60bb4be167d7a905ab8b0e1d3ef

[bootloader] Factor out the command-line for GRUB installation
- ZFS needs environment-setting, but the command is the same,
  so produce the whole command as a list, then pass that onwards.

src/modules/bootloader/main.py
@@ -616,7 +616,6 @@ def run_grub_install(fw_type, partitions, efi_directory, install_hybrid_grub):
     """
 
     is_zfs = any([is_zfs_root(partition) for partition in partitions])
-
     # zfs needs an environment variable set for grub
     if is_zfs:
         check_target_env_call(["sh", "-c", "echo ZPOOL_VDEV_NAME_PATH=1 >> /etc/environment"])
@@ -626,16 +625,19 @@ def run_grub_install(fw_type, partitions, efi_directory, install_hybrid_grub):
         efi_bootloader_id = efi_label(efi_directory)
         efi_target, efi_grub_file, efi_boot_file = get_grub_efi_parameters()
 
+        grubinstall_command = [
+            libcalamares.job.configuration["grubInstall"],
+            "--target=" + efi_target,
+            "--efi-directory=" + efi_directory,
+            "--bootloader-id=" + efi_bootloader_id,
+            "--force"]
+
         if is_zfs:
-            check_target_env_call(["sh", "-c", "ZPOOL_VDEV_NAME_PATH=1 " + libcalamares.job.configuration["grubInstall"]
-                                   + " --target=" + efi_target + " --efi-directory=" + efi_directory
-                                   + " --bootloader-id=" + efi_bootloader_id + " --force"])
+            # Needs environment to be set for GRUB, so go via the shell
+            check_target_env_call(["sh", "-c", "ZPOOL_VDEV_NAME_PATH=1 " + " ".join(grubinstall_command)]
         else:
-            check_target_env_call([libcalamares.job.configuration["grubInstall"],
-                                   "--target=" + efi_target,
-                                   "--efi-directory=" + efi_directory,
-                                   "--bootloader-id=" + efi_bootloader_id,
-                                   "--force"])
+            check_target_env_call(grubinstall_command)
+
     else:
         if libcalamares.globalstorage.value("bootLoader") is None and install_hybrid_grub:
             efi_install_path = libcalamares.globalstorage.value("efiSystemPartition")

MainstreamOS/calamares

commit af5587666f643bce6b724fae92550bd206e765a5

[bootloader] fix syntax error in grubinstall call for ZFS
Fixes a syntax error in ZFS grub installation where a closing parenthesis
was missing from the check_target_env_call function. This bug was
introduced in commit a9c6f3e718 and caused Calamares to fail during
bootloader installation on ZFS systems.

Fixes: #2465

src/modules/bootloader/main.py
@@ -634,7 +634,7 @@ def run_grub_install(fw_type, partitions, efi_directory, install_hybrid_grub):
 
         if is_zfs:
             # Needs environment to be set for GRUB, so go via the shell
-            check_target_env_call(["sh", "-c", "ZPOOL_VDEV_NAME_PATH=1 " + " ".join(grubinstall_command)]
+            check_target_env_call(["sh", "-c", "ZPOOL_VDEV_NAME_PATH=1 " + " ".join(grubinstall_command)])
         else:
             check_target_env_call(grubinstall_command)
 